c62cca306507814be5849982d685eedac4036fea
tangwc <794002197@qq.com>
2022-12-14 21:54:11 +0800

    初始模板

---
+++ main/app_main.h
@@ -0,0 +1,18 @@
+/*
+ * @Description: app_main.h
+ * @Version: v0.0.1
+ * @Autor: tangwc
+ * @Date: 2022-09-25 10:45:12
+ * @LastEditors: tangwc
+ * @LastEditTime: 2022-09-25 10:47:26
+ * @FilePath: \esp32_lvgl\main\app_main.h
+ * 
+ *  Copyright (c) 2022 by tangwc, All Rights Reserved. 
+ */
+#ifndef __APP_MAIN_H__
+#define __APP_MAIN_H__
+
+
+
+
+#endif
